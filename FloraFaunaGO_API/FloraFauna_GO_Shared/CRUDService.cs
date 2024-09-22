0931480a3b9887b6e006037528372bda515e855a
alexis.laurent <Alexis.LAURENT@etu.uca.fr>
2024-09-22 14:17:56 +0200

    + CRUD Service

---
+++ FloraFaunaGO_API/FloraFauna_GO_Shared/CRUDService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraFauna_GO_Shared;
+
+public interface CRUDService<T>
+{
+    T Get(Guid id);
+    bool Delete(Guid id);
+    bool Update(T item);
+    T Create(T item);
+}
