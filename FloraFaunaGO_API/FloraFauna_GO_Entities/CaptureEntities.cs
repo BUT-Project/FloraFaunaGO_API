6baab1a4a2a0e0191b6be4b841a6d3e32dbd34aa
thomas muzard <thomas.muzard@etu.uca.fr>
2024-10-07 14:32:26 +0200

    Add update diagram class

--- FloraFaunaGO_API/FloraFauna_GO_Entities/CaptureEntities.cs
+++ FloraFaunaGO_API/FloraFauna_GO_Entities/CaptureEntities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
@@ -10,39 +11,11 @@ namespace FloraFauna_GO_Entities;
 
 public class CaptureEntities : BaseEntity
 {
-    private Blob photo;
-    public Blob Photo
-    {
-        get { return photo; }
-        set
-        {
-            if (Equals(value, null)) photo = default;
-            else photo = value;
-        }
-    }
+    public Blob Photo { get; set; }
 
-    private uint numero;
-    public uint Numero
-    {
-        get { return numero; }
-        set
-        {
-            if (value < 0) numero = 0;
-            else numero = value;
-        }
-    }
+    public uint Numero { get; set; }
 
-    private List<CaptureDetailsEntities> captureDetails;
+    public ICollection<CaptureDetailsEntities> CaptureDetails = new Collection<CaptureDetailsEntities>();
 
-    public List<CaptureDetailsEntities> CaptureDetails => captureDetails;
-
-    private EspeceEntities espece;
-    public EspeceEntities Espece {
-        get => espece;
-        set
-        {
-            if (value == null || espece == value) return;
-            espece = value;
-        }
-    }
+    public EspeceEntities Espece = new EspeceEntities();
 }
