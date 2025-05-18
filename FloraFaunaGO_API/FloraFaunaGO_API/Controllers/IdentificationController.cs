d09133912c4c6fa0b523065c88bde381d2ffe290
alexislaurent <alexislaurent@ALEXISLAURE0887>
2025-05-18 11:57:51 +0200

    + setup classification python part

--- FloraFaunaGO_API/FloraFaunaGO_API/Controllers/IdentificationController.cs
+++ FloraFaunaGO_API/FloraFaunaGO_API/Controllers/IdentificationController.cs
@@ -9,14 +9,17 @@ namespace FloraFaunaGO_API.Controllers;
 [Route("FloraFaunaGo_API/identification")]
 public class IdentificationController : ControllerBase
 {
+    private readonly IWebHostEnvironment _env;
+
     private readonly ILogger<IdentificationController> _logger;
     public IUnitOfWork<EspeceNormalDto, FullEspeceDto, CaptureNormalDto, FullCaptureDto, CaptureDetailNormalDto, FullCaptureDetailDto, UtilisateurNormalDto, FullUtilisateurDto, SuccessNormalDto, SuccessNormalDto, SuccessStateNormalDto, FullSuccessStateDto, LocalisationNormalDto, LocalisationNormalDto> UnitOfWork { get; private set; }
     public IdentificationService Service { get; private set; }
 
-    public IdentificationController(ILogger<CaptureController> logger, FloraFaunaService service)
+    public IdentificationController(ILogger<CaptureController> logger, FloraFaunaService service, IWebHostEnvironment env)
     {
         UnitOfWork = service;
         Service = new IdentificationService(UnitOfWork.EspeceRepository);
+        _env = env;
     }
 
     [HttpPost("{especeType}")]
@@ -29,5 +32,14 @@ public class IdentificationController : ControllerBase
 
         var result = await Service.identify(dto, type);
         return result != null ? Ok(result) : NoContent();
+    }
+
+    [HttpPost("classify")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> ClassifyImage([FromBody] AnimalIdentifyNormalDto dto)
+    {
+        var result = await Service.classify(dto.AskedImage);
+        return result is EspeceType.None ? Ok(result) : NoContent();
     }
 }
